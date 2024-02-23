e97ca24a8aecacaffa9164b6b8c93320d5c50459
Pacheco117 <68613915+Pacheco117@users.noreply.github.com>
2024-02-23 13:42:19 -0600

    Add files via upload
    first commit

---
+++ WindowsFormsApplication2/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace WindowsFormsApplication2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtNumero = new System.Windows.Forms.TextBox();
+            this.btnA = new System.Windows.Forms.Button();
+            this.lstNumeros = new System.Windows.Forms.ListBox();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // txtNumero
+            // 
+            this.txtNumero.Location = new System.Drawing.Point(44, 59);
+            this.txtNumero.Name = "txtNumero";
+            this.txtNumero.Size = new System.Drawing.Size(100, 20);
+            this.txtNumero.TabIndex = 0;
+            this.txtNumero.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // btnA
+            // 
+            this.btnA.Location = new System.Drawing.Point(166, 59);
+            this.btnA.Name = "btnA";
+            this.btnA.Size = new System.Drawing.Size(122, 23);
+            this.btnA.TabIndex = 1;
+            this.btnA.Text = "Agregar numero";
+            this.btnA.UseVisualStyleBackColor = true;
+            this.btnA.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // lstNumeros
+            // 
+            this.lstNumeros.FormattingEnabled = true;
+            this.lstNumeros.Location = new System.Drawing.Point(44, 85);
+            this.lstNumeros.Name = "lstNumeros";
+            this.lstNumeros.Size = new System.Drawing.Size(118, 225);
+            this.lstNumeros.TabIndex = 3;
+            this.lstNumeros.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(44, 339);
+            this.progressBar.Maximum = 10;
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(320, 42);
+            this.progressBar.TabIndex = 4;
+            this.progressBar.Click += new System.EventHandler(this.progressBar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(414, 477);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.lstNumeros);
+            this.Controls.Add(this.btnA);
+            this.Controls.Add(this.txtNumero);
+            this.Name = "Form1";
+            this.Text = " Ingresa número";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNumero;
+        private System.Windows.Forms.Button btnA;
+        private System.Windows.Forms.ListBox lstNumeros;
+        private System.Windows.Forms.ProgressBar progressBar;
+    }
+}
+
---
+++ WindowsFormsApplication2/Form1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication2
+{
+    public partial class Form1 : Form
+    {
+        private int[] numeros = new int[10];
+        private int contadorNumeros = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            // Cambiar el color de fondo de la ventana
+            this.BackColor = Color.LightGray;
+
+            // Cambiar el tipo de letra y tamaño de la fuente
+            this.Font = new Font("Arial", 12);
+        }
+
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Cambiar el color de fondo de la ventana
+            this.BackColor = Color.LightGray;
+
+            // Cambiar el tipo de letra y tamaño de la fuente
+            this.Font = new Font("Arial", 12);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            // para que solo se ingresen números
+            int num;
+            if (int.TryParse(txtNumero.Text, out num))
+
+            {
+                // Agregar el número al arreglo
+                for (int i = 0; i < numeros.Length; i++)
+                {
+                    if (numeros[i] == 0)
+                    {
+                        numeros[i] = num;
+
+                        contadorNumeros++;
+                        // se llena la progressbar
+                        progressBar.Value = contadorNumeros;
+                        break;
+                    }
+                }
+
+                // ordena los números en el arreglo
+                Array.Sort(numeros);
+
+                if (contadorNumeros <= 10)
+                {
+                    progressBar.Value = contadorNumeros;
+                }
+
+                // agrega los numeros a l listbox
+                lstNumeros.Items.Clear();
+                foreach (int numero in numeros)
+                {
+                    if (numero != 0)
+                        lstNumeros.Items.Add(numero);
+
+                }
+
+               
+                txtNumero.Clear();
+                if (contadorNumeros >= 10)
+                {
+                    btnA.Enabled = false;
+                    this.BackColor = Color.Black;
+
+                    // Cambiar fuente
+                    this.Font = new Font("Comic Sans", 10);
+                }
+
+             
+            }
+
+            else
+            {
+                MessageBox.Show("Por favor, ingrese un número válido.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+
+        }
+
+        private void ActualizarListBox()
+        {
+            lstNumeros.Items.Clear();
+            foreach (int numero in numeros)
+            {
+                if (numero != 0)
+                    lstNumeros.Items.Add(numero);
+            }
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void progressBar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
